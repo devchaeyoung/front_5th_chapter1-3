61ee6e0eef6dc73259ed491f6bc4ebd41dd3970c
devchaeyoung <chaemaa00@gmail.com>
2025-04-11 01:51:01 +0900

    chore: 배포

--- vite.config.ts
+++ vite.config.ts
@@ -3,31 +3,32 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default ({ mode }: { mode: string }) => {
-  const base = mode === "production" ? "/front_5th_chapter1-3/" : "/"; 
+  const base = mode === "production" ? "/front_5th_chapter1-3/" : "/";
 
   return mergeConfig(
-  defineConfig({
-    plugins: [react()],
-    base,
-    build: {
-      rollupOptions: {
-        input: {
-          main: "./index.html",
-          notFound: "./404.html",
+    defineConfig({
+      plugins: [react()],
+      base,
+      build: {
+        rollupOptions: {
+          input: {
+            main: "./index.html",
+            notFound: "./404.html",
+          },
+        },
       },
-    },
-  },
-  }),
-  
-  defineTestConfig({
-    test: {
-      globals: true,
-      environment: "jsdom",
-      setupFiles: "./src/setupTests.ts",
-      coverage: {
-        reportsDirectory: "./.coverage",
-        reporter: ["lcov", "json", "json-summary"],
+    }),
+
+    defineTestConfig({
+      test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: "./src/setupTests.ts",
+        coverage: {
+          reportsDirectory: "./.coverage",
+          reporter: ["lcov", "json", "json-summary"],
+        },
       },
-    },
-  }),
-)};
+    }),
+  );
+};
